4bead60971268eda9b3e4b2b449562e7a2b532b7
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2024-09-08 11:25:38 +0200

    add flake-utils

--- flake.nix
+++ flake.nix
@@ -1,59 +1,65 @@
 {
   description = "NixOS Security Lab";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-  outputs = { self, nixpkgs }: {
-    devShells.default = { system, ... }: 
-      let
-        pkgs = import nixpkgs { inherit system; };
-
-        full = builtins.getEnv "FULL" == "1";       # Include all tools
-        recon = builtins.getEnv "RECON" == "1";     # Relevant tools for reconnaissance
-        scan = builtins.getEnv "SCAN" == "1";       # Relevant tools for scanning for vulnerabilities
-        exploit = builtins.getEnv "EXPLOIT" == "1";
-        exdev = builtins.getEnv "EXDEV" == "1";
-        report = builtins.getEnv "REPORT" == "1";
-
-        packages = 
-          (if recon || full then [ 
-            pkgs.nmap
-            pkgs.bloodhound
-            pkgs.wprecon
-            pkgs.urlhunter
-            pkgs.dnsrecon
-            pkgs.maltego
-          ] else []) ++
-          (if scan || full then [ 
-            pkgs.nmap
-            pkgs.nikto
-            pkgs.zap
-            pkgs.openvas-scanner
-          ] else []) ++
-          (if exploit || full then [ 
-            pkgs.metasploit
-            pkgs.sqlmap
-            pkgs.thc-hydra
-            pkgs.bloodhound
-            pkgs.powersploit
-          ] else []) ++
-          (if exdev || full then [ 
-            (pkgs.python3.withPackages (python-pkgs: [
-              python-pkgs.ropper
-              python-pkgs.pwntools
-              python-pkgs.pycrypto
-            ]))
-            pkgs.aflplusplus
-            pkgs.radare2
-            pkgs.binwalk-full
-            pkgs.gdb
-            pkgs.pwndbg
-            pkgs.binutils
-            pkgs.libc
-          ] else []);
-      in
-      {
-        packages = packages;
-      };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { system = "${system}"; config.allowUnfree = true; };
+          full = false; # Include all tools
+          recon = true; # Relevant tools for reconnaissance
+          scan = false; # Relevant tools for scanning for vulnerabilities
+          exploit = false;
+          exdev = true;
+
+          packages =
+            (if recon || full then [
+              pkgs.nmap
+              pkgs.bloodhound
+              pkgs.wprecon
+              pkgs.urlhunter
+              pkgs.dnsrecon
+              pkgs.maltego
+            ] else [ ]) ++
+            (if scan || full then [
+              pkgs.nmap
+              pkgs.nikto
+              pkgs.zap
+              pkgs.openvas-scanner
+            ] else [ ]) ++
+            (if exploit || full then [
+              pkgs.metasploit
+              pkgs.sqlmap
+              pkgs.thc-hydra
+              pkgs.bloodhound
+              pkgs.powersploit
+            ] else [ ]) ++
+            (if exdev || full then [
+              (pkgs.python3.withPackages (python-pkgs: [
+                python-pkgs.ropper
+                python-pkgs.pwntools
+                python-pkgs.pycrypto
+              ]))
+              pkgs.aflplusplus
+              pkgs.radare2
+              pkgs.binwalk
+              pkgs.gdb
+              pkgs.pwndbg
+              pkgs.binutils
+            ] else [ ]);
+        in
+        {
+          inherit pkgs;
+          devShells.default = pkgs.mkShell
+            {
+              buildInputs = packages;
+            };
+        }
+      );
 }
